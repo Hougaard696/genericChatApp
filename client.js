c5622bde377e4981aa9e49be2f5bf68eae15a27a
Christiaan Hougaard <cwhougaard@gmail.com>
2021-05-16 21:18:01 -0400

    CORS auth added to server and client

--- client.js
+++ client.js
@@ -1,5 +1,10 @@
 ////Code that will be placed inside of your app
-const socket = io('http://localhost:3001')
+const socket = io('http://localhost:3001', {
+  withCredentials: true,
+  extraHeaders: {
+    "my-custom-header": "abcd"
+  }
+})
 
 socket.on('chat-message', data => {
   console.log(data);
--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 require('dotenv').config()
-const io = require('socket.io')(process.env.CHAT_SERVER)
+const io = require('socket.io')(process.env.CHAT_SERVER, {
+  cors: {
+    origin: "http://127.0.0.1:5500",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true
+  }
+})
 
 io.on('connection', socket => {
   socket.emit('chat-message', 'Hello World')
