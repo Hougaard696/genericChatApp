347a6c68c3586ffbc07845578438e18ff4518c98
Christiaan Hougaard <cwhougaard@gmail.com>
2021-05-17 17:48:19 -0400

    squashed bug in html form

--- server.js
+++ server.js
@@ -10,4 +10,7 @@ const io = require('socket.io')(process.env.CHAT_SERVER, {
 
 io.on('connection', socket => {
   socket.emit('chat-message', 'Hello World')
+  socket.on('send-chat-message', message => {
+    console.log(message);
+  })
 })
